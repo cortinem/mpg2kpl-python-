7046d5560603164f0ab595a318cc68970da5bfd6
cortinem <cortinem@dukes.jmu.edu>
2020-10-11 14:49:49 -0400

    best working version

--- converters.py
+++ converters.py
@@ -1,6 +1,26 @@
-# do the simplest thing that could possibly work 
+"""
+converters.py 
+This is a program to convert MPG to KPL.
+The user is asked to enter a number and then 
+the number is converted and the result output 
+to the command line. 
+last update 10-11-20 
+"""
+# declaring known constants 
+KPM = 1.609344     # kilometers per mile 
+GPL = 0.2641720524 # gallons per mile 
+
+def mpg2kpl(mpg):
+    """
+        Converts MPG to KPL via the formula
+        KPL = MPG * KPM* GPL 
+    """
+    return mpg * KPM * GPL   
+
 # ask the user to input an mpg value 
 mpg = input("What is the MPG?")
 #convert the input into a numeric (float) value 
 mpg = float(mpg)
-print(mpg * 1.609344 * 0.2641720524)
+
+# output the converted value 
+print(round (mpg2kpl (mpg), 2), "kpl")
